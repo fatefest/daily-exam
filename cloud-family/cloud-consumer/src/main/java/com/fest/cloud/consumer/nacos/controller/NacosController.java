21b28961530fafcd0fd0a24aa1d7a328a1cc7660
yesitao <fateyst@163.com>
2019-11-11 18:43:16 +0800

    提交

--- cloud-family/cloud-consumer/src/main/java/com/fest/cloud/consumer/nacos/controller/NacosController.java
+++ cloud-family/cloud-consumer/src/main/java/com/fest/cloud/consumer/nacos/controller/NacosController.java
@@ -6,12 +6,9 @@ import com.fest.cloud.dubbo.service.NameService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
@@ -23,12 +20,8 @@ import javax.annotation.Resource;
 @RestController
 public class NacosController {
 
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
     @Resource
     private TestService testService;
-    @Autowired
-    private RestTemplate restTemplate;
     @Autowired
     private ApplicationContext applicationContext;
     @Reference(check = false)
@@ -36,17 +29,12 @@ public class NacosController {
     @Value("${spring.application.name}")
     private String appName;
 
+    @Value("${user.name}")
     private String testName;
 
     @GetMapping("/echo/app-name")
     @SentinelResource
     public String echoAppName() {
-        //Access through the combination of LoadBalanceClient and RestTemplate
-        testName = applicationContext.getEnvironment().getProperty("user.name");
-//        ServiceInstance serviceInstance = loadBalancerClient.choose("service-provider");
-//        String path = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName+"|"+testName);
-//        System.out.println("request path:" +path);
-
         return testService.echo(testName);
     }
 
